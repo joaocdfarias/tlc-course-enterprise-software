8768de26740ce02651858849f54baec3e076ef24
João Farias <joaocdfariass@gmail.com>
2025-10-25 02:05:10 -0300

    test: adds more test cases

--- test/video.e2e-spec.ts
+++ test/video.e2e-spec.ts
@@ -65,5 +65,55 @@ describe('VideoController (e2e)', () => {
           });
         });
     });
+
+    it('throws an error when the thumbnail is not provided', async () => {
+      const video = {
+        title: 'Test Video',
+        description: 'This is a test video',
+        videoUrl: 'uploads/test.mp4',
+        thumbnailUrl: 'uploads/test.jpg',
+        sizeInKb: 1430145,
+        duration: 100,
+      };
+
+      await request(app.getHttpServer())
+        .post('/video')
+        .attach('video', './test/fixtures/sample.mp4')
+        .field('title', video.title)
+        .field('description', video.description)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect((response) => {
+          expect(response.body).toMatchObject({
+            message: 'Both video and thumbnail files are required.',
+            error: 'Bad Request',
+            statusCode: 400,
+          });
+        });
+    });
+
+    it('does not allow non mp4 files', async () => {
+      const video = {
+        title: 'Test Video',
+        description: 'This is a test video',
+        videoUrl: 'uploads/test.mp4',
+        thumbnailUrl: 'uploads/test.jpg',
+        sizeInKb: 100,
+        duration: 100,
+      };
+
+      await request(app.getHttpServer())
+        .post('/video')
+        .attach('video', './test/fixtures/sample.mp3')
+        .attach('thumbnail', './test/fixtures/sample.jpg')
+        .field('title', video.title)
+        .field('description', video.description)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect({
+          message:
+            'Invalid file type. Only video/mp4 and image/jpeg are supported.',
+          error: 'Bad Request',
+          statusCode: 400,
+        });
+    });
   });
 });
