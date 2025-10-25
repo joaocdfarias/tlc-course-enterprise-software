7c22a1ecdd55e8cce58c46cab58fd0141c907e6a
João Farias <joaocdfariass@gmail.com>
2025-10-25 00:18:15 -0300

    chore: reconfigure eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,10 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
