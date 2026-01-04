f257cf84344c1605a65965d6265c2f244dfc88f9
João Farias <joaocdfariass@gmail.com>
2026-01-04 18:33:05 -0300

    feat: uses dtos for validation

--- src/core/service/media-player.service.ts
+++ src/core/service/media-player.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import { VideoNotFoundException } from '../exception/video-not-found.exception';
 
 @Injectable()
 export class MediaPlayerService {
@@ -11,6 +12,11 @@ export class MediaPlayerService {
         id: videoId,
       },
     });
-    return video?.url;
+
+    if (!video) {
+      throw new VideoNotFoundException(`Video with id ${videoId} not found`)
+    }
+
+    return video.url;
   }
 }
