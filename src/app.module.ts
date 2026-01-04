6fcb7ca54cf40e6a50321ea4877d93b99c0e884e
João Farias <joaocdfariass@gmail.com>
2026-01-04 19:50:56 -0300

    feat: adds example with DAO

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ContentController } from '@src/http/rest/controller/content.controller';
 import { PrismaService } from '@src/persistence/prisma/prisma.service';
+import { VideoDAO } from './core/dao/video.dao.interface';
 import { ContentManagementService } from './core/service/content-management.service';
 import { MediaPlayerService } from './core/service/media-player.service';
+import { VideoDAO as VideoDAOImpl } from './persistence/dao/video.dao';
 
 @Module({
   imports: [],
   controllers: [ContentController],
-  providers: [PrismaService, ContentManagementService, MediaPlayerService],
+  providers: [
+    PrismaService,
+    ContentManagementService,
+    MediaPlayerService,
+    {
+      provide: VideoDAO,
+      useClass: VideoDAOImpl,
+    },
+  ],
 })
 export class AppModule {}
